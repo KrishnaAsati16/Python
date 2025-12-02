dfd260ae7f2e02a199e2e83c116a9d04dd787242
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-02 22:14:12 +0530

    Data Types

---
+++ code2.py
@@ -0,0 +1,19 @@
+# data // string 
+name1="sk"
+name2="sk"
+name3="sk"
+
+print(name1)
+print(name2)
+print(name3)
+
+#float = 3.99 // desi-mal
+
+# boolean = true or false 
+
+#none //a = none
+age =69
+old = False 
+a = None
+print(type(old))  #type -> function
+print(type(a))
