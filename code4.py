25be14f9bd475944e45ad310df9c63cbce31573d
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-04 20:48:29 +0530

    str len check

---
+++ code4.py
@@ -0,0 +1,26 @@
+# str1 = "this is a string "
+# str2 = 'IIT dholakpur'
+# str3 = """CSE"""
+# print(str1,str2,str3)
+
+# str1 = "this is a string "
+# str2 = 'IIT dholakpur'
+# str3 = """CSE"""
+# final_str = str1+str2+str3
+# print(final_str)
+
+str1 = "this is a string "
+len1 = len(str1)
+print(len1)
+
+str2 = 'IIT dholakpur'
+len2 = len(str2)
+print(len2)
+
+str3 = "CSE"
+len3 = len(str3)
+print(len3)
+
+final_str = str1+ " "+str2+" "+str3
+print(final_str)
+print(len(final_str))
