b9a8f0de1395b5a721d798bdd3bca3275de7d97b
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-08 16:41:26 +0530

    functions

---
+++ code7.py
@@ -0,0 +1,31 @@
+# functions
+#  endswith-> true or false
+
+# str = "I am studying cobra language"
+# print(str.endswith('uage'))
+
+#  2. capitalise
+
+# str = "i am studying cobra language"
+# print(str.capitalize())
+# print(str)
+
+# 3.replace 
+
+# str = "i am studying cobra language"
+# print(str.replace("cobra","javascript"))
+# print(str)
+
+# 4. find -> returns 1st index of 1st occurrer
+
+# str = "i am studying cobra language"
+# print(str.find("am"))
+# print(str)
+
+# 5.count 
+
+str = "i am studying cobra language"
+print(str.count("a"))
+
+
+
