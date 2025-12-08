4178441c48646b69feb0413a0e5e71a02c2c013e
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-08 16:22:09 +0530

    Slicing

---
+++ code6.py
@@ -0,0 +1,17 @@
+# SLICING -> accessing parts of a string
+# str[starting_idx: ending_idx] 
+# ending idx is not included
+\
+
+
+#str = "global"
+# print(str[0:1])
+
+str = "global"
+print(str[5:]) # [5:len(str)]
+
+
+#special case -> negative index 
+
+str ='apple'
+print(str[-3:-1])
