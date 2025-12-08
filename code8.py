c9378fb3ed8d40bc3d6d690151f5ad887e7a6ea2
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-08 16:42:10 +0530

    Q.

---
+++ code8.py
@@ -0,0 +1,11 @@
+# Question 1-> WAP TO INPUT USER'S FIRST NAME & PRINT ITS LENGTH
+name = input("enter your name")
+print("length of your name is",len(name))
+
+
+
+
+# Question 2-> WAP TO FIND THE OCCURENCE '$' IN A STRING
+
+str = "hi, $i am a $ symbol $90 "
+print(str.count("$"))
