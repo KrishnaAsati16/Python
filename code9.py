e153ba8872d9ee21394dcd88954c02d597cb0db3
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-12 14:27:46 +0530

    if else

---
+++ code9.py
@@ -0,0 +1,20 @@
+# age =20
+
+# if(age>= 18):
+#     print("can vote")
+#     print("can drive")
+            
+
+# num = 5
+# if( num>2):
+#     print("greater than 2")
+
+#     if(num>3):
+#       print("greater than 3")
+
+age = 14
+
+if(age>=18):
+    print("can vote")
+else:
+    print("CANNOT vote")
