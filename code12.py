1fc9976d4d3e18532e8646d547f7ad21cf21dcf0
Krishna Asati <krishnaasati2006@gmail.com>
2025-12-14 20:50:40 +0530

    Method List

---
+++ code12.py
@@ -0,0 +1,32 @@
+# #METHODS IN LIST
+#//1
+
+list= [2,1,30]
+list.append(4)
+print(list)
+
+#//2
+
+list = [2,1,30,5]
+print(list.sort())
+print(list)
+
+#//3
+
+list = [2,1,30,5]
+print(list.sort(reverse=True))
+print(list)
+
+#//4
+
+list = ['a','d','e','f','z','l']
+list.reverse()
+print(list) 
+
+#//5
+
+list = [2,1,3,1]
+list.pop(2)
+print(list)
+
+
