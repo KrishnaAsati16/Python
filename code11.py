cbcfccad726bb2fcfa4c1fa83ef3431f16e750f5
Krishna Asati <krishnaasati2006@gmail.com>
2025-12-14 20:50:07 +0530

    List in python

---
+++ code11.py
@@ -0,0 +1,14 @@
+# marks = 94.4
+# marks2 = 85
+# marks3 = 66
+# marks4= 96
+# marks5 = 45.5
+
+# LISTS IN PYTHON->
+# A built in data type that is store set of values it can store elements of different types(int,float,string etc)
+# in this slicing can be done 
+
+marks= [94.4, 87.5, 95.2,66.4, 45.1]
+print(marks)
+print(type(marks))
+print(marks[0])
