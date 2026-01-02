e7bacf449ddcce275eeba21728e666318ccd5393
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-02 23:06:12 +0530

    Questions

---
+++ code14.py
@@ -0,0 +1,32 @@
+# WAP TO ASK The user to enter name of their three favorite movies stores In a list
+# movies = []
+# mov1 = input(" enter 1st movie:")
+# mov2 = input(" enter 2nd movie:")
+# mov3 = input(" enter 3rd movie:")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+
+# Q. We have to check if a list contains a palindrome of element
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+
+copy_list1 = list1.copy()
+copy_list1.reverse
+
+if(copy_list1 == list1):
+    print("palindrome")  
+else:
+    print("Not palindrome")
+
+
+    #Q. Wap to count the number of student with a grades in following tule
+  
+# grade = ("C";"D","A","A","B","B","A")
+# print(grade.count("A"))
+
