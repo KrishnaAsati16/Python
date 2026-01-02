f8a210c44fe0ce3be1cfdc8daff21be770f0bcc6
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-02 23:06:43 +0530

    Dictonary

---
+++ code15.py
@@ -0,0 +1,23 @@
+# Dictionary in Python-> 
+#  Dictinory aer used to store value in key: value pairs .They are unorder , mutable (changeable) & dont allow duplicate keys 
+# dict = {
+#     "name" : krishna ,
+#     " cgpa " : 9.6        // key value pairs
+#     "marks": [99,97,98]
+# }
+
+
+info = {
+    "key" : " value",
+    "name": "krishna ",
+    "Learning" : " python"
+}
+print(info)
+
+
+info["name"] = "KRISHNA"  # overwrite
+info["key"] = "true"
+print(info)
+
+null_dict = {} 
+print(null_dict)  # null dictonary
