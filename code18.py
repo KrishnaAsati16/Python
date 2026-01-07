f6c94f3bd58a6043051bdd2c8f497f0dc4a5ddf0
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-07 21:44:55 +0530

    Set In Python

---
+++ code18.py
@@ -0,0 +1,15 @@
+# SET IN PYTHON ->
+# Set is the collection of the unordered items. Each element in the set must be unique & immutable 
+
+# collection = { 1,2,2,2, " hello ", " world ", " world ", }
+
+# print(collection)
+# print(len(collection))
+
+# collection = {} # empty dictonary
+
+# print(type(collection))
+
+collection = set()  # empty set; dictonary
+
+print(type(collection))
