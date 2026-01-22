ac21776d3250c719e308cd78c0575ceddf852e64
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-22 16:52:53 +0530

    FOR

---
+++ code23.py
@@ -0,0 +1,16 @@
+# veggies = ["potatao" ,'brijal','ladyfingure']
+
+# for val in veggies :
+#     print(val)
+
+# tup = (1,2,3,4,5,6,7)
+# for num in tup:
+#         print(num)
+
+
+str = "apna college"
+
+for char in str :
+    print(char)
+else:
+    print("end")
