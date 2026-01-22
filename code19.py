c2a5a3d5dcf2fb222628abc72dd339c71b7c46db
Krishna Asati <krishnaasati2006@gmail.com>
2026-01-22 16:38:33 +0530

    loop

---
+++ code19.py
@@ -0,0 +1,23 @@
+
+# count = 1
+# while count<=5 :
+#     print('hello')
+#     count+=1
+
+# i=1
+# while i<=1000000:
+#         print('hello',i)
+#         i+=1
+ 
+# print number from 1 to 5
+# i=5
+# while i<=5:
+#     print(i)
+#     i+=1
+#     print("end loop")
+
+i=5
+while i>=1:
+    print(i)
+    i-=1
+    
